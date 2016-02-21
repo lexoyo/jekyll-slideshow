08e12fe782da031481eb64a61eebea5a5d39c48a
Alex Hoyau <a.hoyau@silexlabs.org>
2016-02-21 03:00:53 +0100

    little UX fixes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
 $(function () {
-	$('a.embed').click(function () {
+	$('a.embed').on('click', function () {
 		$(this.parentNode).children('.embed-pane').toggle();
 		return false;
+	});
+	$('.embed-input').on("click", function () {
+    $(this).select();
 	});
 });
